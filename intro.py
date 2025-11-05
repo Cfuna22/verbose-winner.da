c6036c19eef9400f332853e127079b93b493dece
Cfuna22 <abelsifuna515@gmail.com>
2025-11-06 01:09:07 +0300

    done

---
+++ intro.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Create a simple series
+s = pd.Series([10, 20, 30, 40,50])
+
+print(s)
+print("Value1", s.values)
+print("Value2", s.index)
--- sales.py
+++ sales.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-# Load dataset
-df = pd.read_csv("sales.csv")
+# create DataFrames
+data = {
+    'Name': ['alice', 'Bob', 'Charlie', 'Diana'],
+    'Age': [25, 30, 25, 28],
+    'City': ['New York', 'London', 'Tokyo', 'Paris'],
+    'Salary': [50000, 60000, 70000, 55000]
+}
+df = pd.DataFrame(data)
+print("Original DataFrame")
+print(df)
 
 # Display first few rows
 print(df.head())
